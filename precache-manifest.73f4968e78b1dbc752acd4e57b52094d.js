1de622e3bf0972f874ce5b79b7365ec7db4fb544
Neetsel <55947537+Neetsel@users.noreply.github.com>
2020-10-04 19:08:08 +0200

    Updates

---
+++ precache-manifest.73f4968e78b1dbc752acd4e57b52094d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78599b52bd027aaf8b1f22453a6b570",
+    "url": "/the-rolistes-pod/index.html"
+  },
+  {
+    "revision": "73de4056e82e61998976",
+    "url": "/the-rolistes-pod/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da5e2139613bc9506c2e",
+    "url": "/the-rolistes-pod/static/js/2.2de91074.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-rolistes-pod/static/js/2.2de91074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73de4056e82e61998976",
+    "url": "/the-rolistes-pod/static/js/main.1a8276e0.chunk.js"
+  },
+  {
+    "revision": "b78c7a92ee0f0f9885aa",
+    "url": "/the-rolistes-pod/static/js/runtime-main.a50c7f5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-rolistes-pod/static/media/logo.5d5d9eef.svg"
+  }
+]);
