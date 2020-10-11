57c9dd4ea45f9553304761a6007c71c3ca1717ab
Neetsel <55947537+Neetsel@users.noreply.github.com>
2020-10-11 21:29:59 +0200

    removed axios

--- src/hooks/store/actions/post.js
+++ src/hooks/store/actions/post.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-import axios from 'axios';
+
 
 export const fetchPostStart = () => {
     return {
@@ -28,14 +28,9 @@ export const fetchPosts = (token) => {
         
         // let parser = new xml2js.Parser();
         // parser.parseString
+
+
         
-        axios.get('https://firebasestorage.googleapis.com/v0/b/the-rolistes-pod.appspot.com/o/therolistespodcast.post.xml?alt=media&token=278cc4b9-ac26-425e-8eb0-b7882c2d453d',{"Content-Type": "application/xml; charset=utf-8"})
-            .then(res => {
-                console.log(res.data);
-                // dispatch(fetchPostSuccess());
-            })
-            .catch(err => {
-                // dispatch(fetchPostFailed());
-            });
+       
     }
 }
