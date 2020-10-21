54153dda7a6646a21872dc0df860b85114b7333e
Neetsel <55947537+Neetsel@users.noreply.github.com>
2020-10-21 19:59:47 +0200

    Worked on sidebar icons

--- src/components/side/Follow/Follow.js
+++ src/components/side/Follow/Follow.js
@@ -1,6 +1,11 @@
+import { faApple, faFacebookF, faInstagram, faItunes, faSpotify, faTiktok, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faPodcast } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import classes from './Follow.module.css'
 import { Row, Col } from 'react-bootstrap';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
+
 
 const Follow = (props) => {
     return (
@@ -11,40 +16,49 @@ const Follow = (props) => {
                 </Col>
             </Row>
             <Row>
-                <h3>Follow</h3>
-                <Col xs={3}>
-                    <a href="https://www.facebook.com/Rolistespod/" class="side-link"><i class="fab fa-facebook-f icon-side" aria-hidden="true"></i></a>
+                <Col xs={12}>
+                    <h3>Follow</h3>
                 </Col>
+                <Row>
+                    <Col xs={3}>
+                        <a href="https://www.facebook.com/Rolistespod/" className={classes.SideLink}><span><FontAwesomeIcon icon={faFacebookF} aria-hidden="true"/></span></a>
+                    </Col>
 
-                <Col xs={3}>
-                    <a href="https://twitter.com/rolistespod" class="side-link"><i class="fab fa-twitter icon-side" aria-hidden="true"></i></a>
-                </Col>
+                    <Col xs={3}>
+                        <a href="https://twitter.com/rolistespod" className={classes.SideLink}><span><FontAwesomeIcon icon={faTwitter} aria-hidden="true"/></span></a>
+                    </Col>
 
-                <Col xs={3}>
-                    <a href="https://www.instagram.com/rolistespod/?hl=en" class="side-link"><i class="fab fa-instagram icon-side" aria-hidden="true"></i></a>
-                </Col>
+                    <Col xs={3}>
+                        <a href="https://www.instagram.com/rolistespod/?hl=en" className={classes.SideLink}><span><FontAwesomeIcon icon={faInstagram} aria-hidden="true"/></span></a>
+                    </Col>
 
-                <Col xs={3}>
-                <a href="https://www.tiktok.com/@rolistespod?lang=en" class="side-link"><i class="fab fa-tiktok icon-side" aria-hidden="true"></i></a>
-                </Col>
+                    <Col xs={3}>
+                        <a href="https://www.tiktok.com/@rolistespod?lang=en" className={classes.SideLink}><span><FontAwesomeIcon icon={faTiktok} aria-hidden="true"/></span></a>
+                    </Col>
+                </Row>
+                
             </Row>
             <Row>
-            <h3>Listen</h3>
-                <Col xs={3}>
-                    <a href="https://open.spotify.com/show/6WC5wywaLMEhnO7JWIuxom?si=f8oaIyG1QU2cgrBeDlLP5A" class="side-link"><i class="fa fa-spotify icon-side" aria-hidden="true"></i></a>
+                <Col xs={12}>
+                    <h3>Listen</h3>
                 </Col>
+                <Row>
+                    <Col xs={3}>
+                        <a href="https://open.spotify.com/show/6WC5wywaLMEhnO7JWIuxom?si=f8oaIyG1QU2cgrBeDlLP5A" className={classes.SideLink}><span><FontAwesomeIcon icon={faSpotify} aria-hidden="true"/></span></a>
+                    </Col>
 
-                <Col xs={3}>
-                    <a href="https://podcasts.apple.com/us/podcast/the-rolistes-podcast/id1043891191" class="side-link"><i class="fa fa-apple icon-side" aria-hidden="true"></i></a>
-                </Col>
+                    <Col xs={3}>
+                        <a href="https://podcasts.apple.com/us/podcast/the-rolistes-podcast/id1043891191" className={classes.SideLink}><span><FontAwesomeIcon icon={faApple} aria-hidden="true"/></span></a>
+                    </Col>
 
-                <Col xs={3}>
-                    <a href="https://www.youtube.com/channel/UCpguBUBlPRHN4BGTbKPFxrg" class="side-link"><i class="fab fa-youtube icon-side" aria-hidden="true"></i></a>
-                </Col>
+                    <Col xs={3}>
+                        <a href="https://www.youtube.com/channel/UCpguBUBlPRHN4BGTbKPFxrg" className={classes.SideLink}><span><FontAwesomeIcon icon={faYoutube} aria-hidden="true"/></span></a>
+                    </Col>
 
-                <Col xs={3}>
-                    <a href="http://www.stitcher.com/podcast/the-rolistes-podcast" class="side-link"><i class="fa fa-podcast icon-side" aria-hidden="true"></i></a>
-                </Col>
+                    <Col xs={3}>
+                        <a href="http://www.stitcher.com/podcast/the-rolistes-podcast" className={classes.SideLink}><span><FontAwesomeIcon icon={faPodcast} aria-hidden="true"/></span></a>
+                    </Col>
+                </Row>
             </Row>
         </Aux>
     )
--- src/components/side/Support/Support.js
+++ src/components/side/Support/Support.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import paypalButton from '../../../assets/images/paypal-button.png';
+import patreonButton from '../../../assets/images/patreon-button.png';
+import classes from './Support.module.css';
 
 const Support = (props) => {
     return (
         <Aux>
             <Row>
-                <h2 class="section-title-side"><span class="text-title-side">Support Us</span></h2>
-                <Col xs={6}>
-                    <a href="https://www.patreon.com/RolistesPod" class="support-link-side"><i class="fab fa-patreon icon-side support-icon" aria-hidden="true"></i></a>
+                <Col xs={12}>
+                    <h2 class="section-title-side"><span class="text-title-side">Support Us</span></h2>
+                </Col>
+                
+                <Col xs={12}>
+                    <a href="https://www.patreon.com/RolistesPod" class="support-link-side"><img src={patreonButton} alt="Support me on Patreon" aria-hidden="true"/></a>
                 </Col>
 
-                <Col xs={6}>
-                    <a href="https://www.patreon.com/RolistesPod" class="support-link-side"><i class="fab fa-paypal icon-side support-icon" aria-hidden="true"></i></a>
+                <Col xs={12}>
+                    <a href="https://www.paypal.com/paypalme/rolistespod/" class="support-link-side"><img src={paypalButton}  alt="Donate with Paypal" aria-hidden="true"/></a>
                 </Col>                    
             </Row>    
         </Aux>
