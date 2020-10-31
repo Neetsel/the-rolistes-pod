b3b60c011ec804b7aabd27010f2039e97fa46f9e
Neetsel <neetsel@gmail.com>
2020-10-31 18:04:52 +0100

    only put podcast in xml

--- generateRSSFeed.js
+++ generateRSSFeed.js
@@ -3,15 +3,49 @@ const fs = require ('fs');
 var axios = require('axios');
 // const xmlData = require('./src/assets/therolistespodcast.xml');
 
-const getPosts = () => {        
+function toRssXML(posts) {    
+    // console.log(posts); 
+
+    const latestPostDate = new Date(posts[0]["pubDate"], "MM/DD/YYYY"); 
+    let postXml = "";
+
+    posts.forEach((post) => {
+        const date = new Date(post.pubDate, "MM/DD/YYYY");
+        // const href = `https://www.slashproto.com/posts/${post.slug}`;
+        const href = `https://www.slashproto.com/posts/`;
+        postXml += `
+        <item>
+            <title>${post.title}</title>
+            <link>${href}</link>
+            <pubDate>${post.pubDate}</pubDate>
+            <guid>${href}</guid>
+            <description>
+            &lt;![CDATA[ ${post.description} ]]&gt;
+            </description>
+        </item>`;
+    });
+
+    return `<?xml version="1.0" ?>
+    <rss version="2.0">
+        <channel>
+            <title>/proto - Protocol7's Blog</title>
+            <link>https://www.slashproto.com/</link>
+            <description>kjioji</description>
+            <language>en</language>
+            <lastBuildDate>${latestPostDate}</lastBuildDate>
+            ${postXml}
+        </channel>
+    </rss>`;
+}
+
+function getPosts () {    
+    
+    const fetchedPosts = [];
+    const fetchedPodcast = [];
     
     fs.readFile('./src/assets/therolistespodcast.xml', function(err, data){
-        console.log();
         let xml2js = require('xml2js');
-        let parser = new xml2js.Parser();
-        
-        const fetchedPosts = [];
-        const fetchedPodcast = [];
+        let parser = new xml2js.Parser();            
 
         parser.parseString(
             data,
@@ -44,15 +78,17 @@ const getPosts = () => {
 
                         for (let i=0; i < fetchedPosts[key]["category"].length; i++) {
                             
+                            console.log(i);
+
                             switch(fetchedPosts[key]["category"][i]["$"]["nicename"]){
 
                                 case "podcast": 
                                     fetchedPodcast.push({
-                                    title:fetchedPosts[key],
-                                    link:fetchedPosts[key],
-                                    pubDate:fetchedPosts[key],
-                                    guid:fetchedPosts[key],                                    
-                                    description:fetchedPosts[key],
+                                    title:fetchedPosts[key]["title"],
+                                    link:fetchedPosts[key]["title"],
+                                    pubDate:fetchedPosts[key]["pubDate"][0],
+                                    guid:fetchedPosts[key]["title"],                                    
+                                    description:fetchedPosts[key]["title"],
                                     id:key                                                
                                     });
 
@@ -64,47 +100,28 @@ const getPosts = () => {
 
                 fetchedPosts.reverse();
                 fetchedPodcast.reverse();
-                console.log("fgfd");                
-                return fetchedPodcast;                                 
+                // console.log(fetchedPodcast); 
+
+                const xml = toRssXML(fetchedPodcast);
+                // const xml = 'test';
+                fs.writeFileSync("./public/rss.xml", xml);                                                                            
             }
            
         )
     }) 
-}
+    console.log(fetchedPodcast);
 
-function toRssXML(posts) {
-    const latestPostDate = moment(posts[0].metadata.date, "MM/DD/YYYY");
-    let postXml = "";
-
-    posts.forEach((post) => {
-        const date = moment(post.metadata.date, "MM/DD/YYYY");
-        const href = `https://www.slashproto.com/posts/${post.slug}`;
-        postXml += `
-        <item>
-            <title>${post.metadata.title}</title>
-            <link>${href}</link>
-            <pubDate>${date.format("ddd, DD MMM YYYY HH:mm:ss ZZ")}</pubDate>
-            <guid>${href}</guid>
-            <description>
-            &lt;![CDATA[ ${post.metadata.description} ]]&gt;
-            </description>
-        </item>`;
-    });
-
-    return `<?xml version="1.0" ?>
-    <rss version="2.0">
-        <channel>
-            <title>/proto - Protocol7's Blog</title>
-            <link>https://www.slashproto.com/</link>
-            <description>kjioji</description>
-            <language>en</language>
-            <lastBuildDate>${latestPostDate.format("ddd, DD MMM YYYY HH:mm:ss ZZ")}</lastBuildDate>
-            ${postXml}
-        </channel>
-    </rss>`;
+    // console.log('test');
+    // console.log(posts);
+    
+ 
 }
 
 const posts = getPosts();
+// console.log('test');
+// console.log(posts);
 // const xml = toRssXML(posts);
-const xml = 'test';
-fs.writeFileSync("./public/rss.xml", xml);
+// const xml = 'test';
+// fs.writeFileSync("./public/rss.xml", xml);
+ 
+
