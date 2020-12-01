060555ca5763bf281c97ee1606f37971cb5472b4
Neetsel <55947537+Neetsel@users.noreply.github.com>
2020-12-01 20:47:08 +0100

    added new logo

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,10 +5,16 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems= (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/news">News</NavigationItem>
+        <span className={classes.VerticalLine}/>
         <NavigationItem link="/podcast">Podcast</NavigationItem> 
+        <span className={classes.VerticalLine}/>
         <NavigationItem link="/paris_gondo">Paris Gondo</NavigationItem>
+        <span className={classes.VerticalLine}/>
         <NavigationItem link="/about_us">About Us</NavigationItem>
+        
+        
     </ul>
+
 );
 
 export default navigationItems;
