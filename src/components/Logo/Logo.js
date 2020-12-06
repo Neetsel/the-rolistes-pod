1803d8ea6cac8d6de993614cbc41394357bb6841
Neetsel <neetsel@gmail.com>
2020-12-06 21:19:48 +0100

    added second image nav

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Logo.module.css';
-import imgLogo from '../../assets/images/Logo_Nav.png';
+import imgLogo from '../../assets/images/Logo_Nav_Small.jpg';
+import txtLogo from '../../assets/images/Logo_Nav_Title.jpg';
 
 const logo = (props) => (
     <div className={classes.Logo}>
         <img src={imgLogo} alt="Logo"/>
+        <img src={txtLogo} alt="The Rolistes"/>
     </div>
 );
 
