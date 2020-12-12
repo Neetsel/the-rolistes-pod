e15a4f9d09aa2bd9412f171b1ba9be6a47fe339b
Neetsel <neetsel@gmail.com>
2020-12-12 12:03:21 +0100

    removed oversized images

--- src/components/ComingSoon/ComingSoon.js
+++ src/components/ComingSoon/ComingSoon.js
@@ -8,6 +8,7 @@ import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 const ComingSoon = (props) => (
     <Aux>
+        <img src="" alt="" />
         <div className={[globalClasses.item_box, classes.item_box_comingsoon].join(' ')}>
             <h2>Coming Soon</h2>
             <div className={classes.date}><FontAwesomeIcon icon={faCalendarAlt} aria-hidden="true"/>09 July, 2020 1pm EDT (6pm BST)</div>
